3ff57f7bd91bb5e68ce9055795fc0471df81b4ad
Gyen Abubakar Sadick <gyen@walulel.com>
2020-12-16 13:54:09 +0000

    Implemented the game block.
    * Added a button to start game.
    * Designed the div.block element and all of its logic.

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,60 @@
 <template>
-  <h1>Reaction Timer Game</h1>
+	<h1>Reaction Timer Game</h1>
+
+	<button class="control" @click="startGame" :disabled="isPlaying">
+		<span>Start game!</span>
+		<span>🔥</span>
+	</button>
+
+	<Block v-if="isPlaying" :delay="delay" @gameEnded="handleGameEnded" />
 </template>
 
 <script>
-export default {
-  name: 'App',
-  components: {
+import Block from "./components/Block";
 
-  }
-}
+export default {
+	name: "App",
+	components: {
+		Block,
+	},
+	data() {
+		return {
+			isPlaying: false,
+			delay: null,
+			showResults: false
+		};
+	},
+	methods: {
+		startGame() {
+			this.delay = 2000 + Math.random() * 5000;
+			this.isPlaying = true;
+		},
+		handleGameEnded(results) {
+
+		}
+	},
+};
 </script>
 
 <style>
-
+.control {
+	border: 1px solid rgb(67, 21, 192);
+	outline: 0;
+	background: transparent;
+	font-size: 16px;
+	padding: 10px 20px;
+	border-radius: 5px;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	margin: 0 auto 2em;
+	cursor: pointer;
+}
+.control span:last-child {
+	margin-left: 10px;
+}
+.control:disabled {
+	border-color: lightgray;
+	cursor: not-allowed;
+}
 </style>
---
+++ src/components/Block.vue
@@ -0,0 +1,59 @@
+<template>
+	<div class="block" v-if="showBlock" @click="stopTimer">
+		<p>Click here</p>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "Block",
+	props: ["delay"],
+
+	data() {
+		return {
+			showBlock: false,
+			timer: null,
+			reactionTime: 0,
+		};
+	},
+
+	mounted() {
+		setTimeout(() => {
+			this.showBlock = true;
+			this.startTimer();
+		}, this.delay);
+	},
+
+	methods: {
+		startTimer() {
+			this.timer = setInterval(() => {
+				this.reactionTime += 10;
+			}, 10);
+		},
+
+		stopTimer() {
+			clearInterval(this.timer);
+			this.$emit('gameEnded', { reactionTime })
+		},
+	},
+};
+</script>
+
+<style>
+.block {
+	width: 300px;
+	height: 300px;
+	margin: 0 auto;
+	background: rgb(67, 21, 192);
+	color: #fff;
+	border-radius: 15px;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	cursor: pointer;
+}
+
+h1 {
+	text-align: center;
+}
+</style>
